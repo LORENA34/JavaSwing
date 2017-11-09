21022651bd93c49631c9805d06b899c6726a5af2
lorena salinas <nena_sr83@hotmail.com>
2017-11-08 20:11:04 -0400

    importamos la libreria

---
+++ src/javaswing/JavaSwing.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaswing;
+//PASO 1 - IMPORTAMOS LAS LIBRERIAS
+import javax.swing.*;//Importamos TODO de swing
+import java.awt.*;
+/**
+ *
+ * @author LAB15
+ */
+//PASO 2- HEREDAR LA CLASE DESDE JFRAME
+public class JavaSwing extends JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
